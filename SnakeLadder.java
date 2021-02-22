efa320c27641f4c4a44cd9734813738abb572e7a
unknown <kumar.skm03@gmail.com>
2021-02-22 11:06:07 +0530

    added player at start position in SnakeLadder.java

---
+++ SnakeLadder.java
@@ -0,0 +1,9 @@
+
+public class SnakeLadder {
+	public static final int START_POSITION = 0;
+	public static void main(String args[]) {
+
+		int playerPosition = START_POSITION;
+		System.out.println("Position of player: " + playerPosition);
+	}
+}
