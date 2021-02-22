b2997569273e3a53943873a6592c4836f15222ac
unknown <kumar.skm03@gmail.com>
2021-02-22 11:20:36 +0530

    added dice in SnakeLadder.java

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,6 +4,8 @@ public class SnakeLadder {
 	public static void main(String args[]) {
 
 		int playerPosition = START_POSITION;
+		int diceValue = (int) (Math.random() * 6) + 1;
+		playerPosition = diceValue;
 		System.out.println("Position of player: " + playerPosition);
 	}
 }
